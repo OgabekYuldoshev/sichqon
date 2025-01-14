f9bef8a852b753cb27cbca2ae958070744ba3ed7
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-01-14 16:23:08 +0500

    feat(lib): createTunnel function is done

---
+++ src/index.tsx
@@ -0,0 +1,50 @@
+import {
+    type PropsWithChildren,
+    type ReactNode,
+    useEffect,
+    useLayoutEffect,
+    useSyncExternalStore,
+} from "react";
+
+const useIsomorphicLayoutEffect =
+    typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+type Listener = () => void;
+function createTunnel() {
+    let localState: ReactNode[] = [];
+    let listeners: Listener[] = [];
+
+    function subscribe(listener: Listener) {
+        listeners = [...listeners, listener];
+
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        };
+    }
+
+    function notifyListeners() {
+        for (const listener of listeners) {
+            listener();
+        }
+    }
+
+    return {
+        In({ children }: PropsWithChildren) {
+            useIsomorphicLayoutEffect(() => {
+                localState = [...localState, children]
+                notifyListeners();
+                return () => {
+                    localState = localState.filter((c) => c !== children);
+                }
+            }, [children]);
+
+            return null
+        },
+        Out() {
+            const currentState = useSyncExternalStore(subscribe, () => localState)
+            return <>{currentState}</>
+        },
+    };
+}
+
+export { createTunnel }
